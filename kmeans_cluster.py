bf70a2556473b195ca674b3d29f1b9fce1736f4d
maurogwerder <mauro.gwerder@students.unibe.ch>
2020-10-05 14:51:44 +0200

    included mask

--- kmeans_cluster.py
+++ kmeans_cluster.py
@@ -2,6 +2,8 @@ from sklearn import cluster
 import numpy as np
 import cv2
 import os
+from matplotlib import pyplot as plt
+import numpy.ma as ma
 
 
 def main():
@@ -18,13 +20,20 @@ def main():
         # Read the data as greyscale
         img = cv2.imread(MYPATH + file, 0)
         # Group similar grey levels using 8 clusters
-        values, labels = km_clust(img, n_clusters=5)
+        values, labels = km_clust(img, n_clusters=8)
         # Create the segmented array from labels and values
         img_segm = np.choose(labels, values)
         # Reshape the array as the original image
         img_segm.shape = img.shape
+        # We sort the values: the cochlea is falling in the second darkest category, so we take the second value
+        values.sort()
+        print(values)
+        mask = np.where((img_segm >= values[6]), True, False)
+        masked_img = ma.masked_array(img_segm, mask)
+        plt.imshow(masked_img)
+        plt.show()
 
-        cv2.imwrite(os.getcwd() + f'/img_kmeans/{file[:1]}_kmeans{file[1:]}', img_segm)
+        cv2.imwrite(os.getcwd() + f'/img_kmeans/{file[:1]}_kmeans_masked_{file[1:]}', masked_img)
 
 
 # function from https://www.idtools.com.au/segmentation-k-means-python/
