5a1856e4b6fe22b861ebaaac1c336f5524e86f67
maurogwerder <mauro.gwerder@students.unibe.ch>
2020-10-07 14:07:31 +0200

    update parameters

--- hough_circle.py
+++ hough_circle.py
@@ -31,13 +31,11 @@ def main():
     for i in range(len(fnames)):
         for file in fnames[i]:
             img_circle = cv2.imread(f'{MYPATH}/img_{folders[i]}/{file}', 0)
-
-            # Copy file for visualization of false-positive errors
-            img_error = np.copy(img_circle)
             img_mask = cv2.imread(f'{MYPATH}/img_kmeans/masks/{fnames_masks[i]}', 0)
 
             # Applies houghtransform to image
             circles = cv2.HoughCircles(img_circle, cv2.HOUGH_GRADIENT, 0.9, 25, param1=23, param2=25, maxRadius=25)
+            img_circle = np.stack((img_circle, img_circle, img_circle), axis=2)
             circles_rounded = np.uint16(np.around(circles))
 
             arr_of_hits = None  # Will hold coordinates and radius of each hit per file
@@ -45,8 +43,8 @@ def main():
 
                 # Only include hit if the mask is not covering it
                 if img_mask[j[1], j[0]] == 0:
-                    cv2.circle(img_circle, (j[0], j[1]), 2, (255, 0, 0), 3)
-                    cv2.circle(img_error, (j[0], j[1]), 2, (0, 0, 0), 3)
+
+                    cv2.circle(img_circle, (j[0], j[1]), 2, (0, 255, 0), 4)
 
                     if arr_of_hits is None:
                         arr_of_hits = np.array((j[0], j[1], j[2])).reshape(1, 3)
@@ -64,9 +62,8 @@ def main():
             df_image.to_csv(f'{MYPATH}/csv_files/{file[:-4]}.csv')
 
             df_complete = pd.DataFrame(list_of_files, columns=['file_name', 'num_of_hits'])
-            df_complete.to_csv(f'{MYPATH}/csv_files/all_files.csv')
+            df_complete.to_csv(f'{MYPATH}/all_files.csv')
             cv2.imwrite(f'{MYPATH}/img_hough/{folders[i]}/{file[:-4]}_hough.jpg', img_circle)
-            cv2.imwrite(f'{MYPATH}/img_hough/{folders[i]}/{file[:-4]}_houghErrors.jpg', img_error)
 
 
 
